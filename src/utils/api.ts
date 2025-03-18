3710d3551adaa566e3068fdfa3a4264f2841f6ca
nishant-zeus <nishantpal1409@gmail.com>
2025-03-18 23:47:28 +0530

    https issue fix - trial 1

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.60.210.227:8080/api", // Replace with your backend URL
+  baseURL: "/api", // Replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
